289f5a6a3dc07a3602ba0325b2493e65c8e42a66
Szymon <szymondressler@gmail.com>
2021-09-26 08:57:18 +0200

    ćwiczenie 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("To moj pierwszy program w jezyku Java");
+        System.out.println("Mam nadzieje" + " " + "ze " + "nauka bedzie przyjemna!");
+
+
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
