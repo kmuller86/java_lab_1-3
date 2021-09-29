9b16073c785f8484101fcfb1665f80200f39ad9d
Szymon <szymondressler@gmail.com>
2021-09-29 11:27:52 +0200

    ćwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,24 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
+        int a = 23;
+        int b = 11;
+        int c = a + b;
+        System.out.println(c);
+        c = a - b;
+        System.out.println(c);
+        c = a * b;
+        System.out.println(c);
+        c = a / b;
+        System.out.println(c);
+        c = a % b;
+        System.out.println(c);
+
+
+
+        boolean result = a == b;
+        System.out.println(result);
+        System.out.println(a >= 0);
+        System.out.println(b != 10);
     }
 }
