575b573fdd7cbf234f277326d53eb71be5202433
kmuller86 <89707079+kmuller86@users.noreply.github.com>
2022-01-17 00:21:37 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,12 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
         double[] tablica = new double[5];
+        /*
+        for (int i = 0; i < 5; i++) {
+            tablica [i] = i +1.66;
+         }
+         Iterator tablicy (tab[i]) musi być zawsze liczbą calkowitą, był pan blisko dobrego rozwiązania 
+        */
         for (double i = 0; i < 5; i++)
             tablica [(int) i] = i +1;
         double zmienna = tablica[3];
